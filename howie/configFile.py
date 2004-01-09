57a4a624e1c74ec183a41def83450e16af01aebd
cort <cort>
2004-01-09 18:24:35 +0000

    Changed __ prefix to _.  Added new default parameters for sessionspersist and sessionsdir.  Automatically load the config file if it hasn't been loaded the first time get() is called.

--- howie/configFile.py
+++ howie/configFile.py
@@ -12,6 +12,8 @@ import ConfigParser
 from howie import frontends
 from frontends import *
 
+configFileName = "howie.ini"
+
 # Load default values for various config-file options.
 # The defaults are scattered throughout the frontend and
 # backend modules, each of which may have a dictionary
@@ -19,10 +21,14 @@ from frontends import *
 # below.  This code merges all the configDefaults into
 # one master copy.
 configDefaults = {
-    "general.botname":		"Howie",
-    "general.botmaster":	"Your Name",
-    "general.maxdelay":     "0",
-    "general.verbose":      "no",
+    "general.botname":		        "Howie",
+    "general.botmaster":	        "Your Name",
+    "general.logging":              "no",
+    "general.logdir":               "logs",
+    "general.maxdelay":             "0",
+    "general.sessionspersist":      "no",
+    "general.sessionsdir":          "sessions",
+    "general.verbose":              "no",
     }
 for package in ['frontends']:
     for name in eval("%s.__all__" % package):
@@ -35,66 +41,68 @@ for package in ['frontends']:
             configDefaults[k] = v
 
 
-def __WriteINI(file, config=configDefaults):
-	"""
-	Writes the config dictionary to a .INI file.
-	"""
-	if len(config) == 0:
-		return
-	iniFile = open(file, "w")
-	currentSection = ""
-	keys = config.keys()
-	keys.sort()
-	for key in keys:
-		# Don't write entries with no value
-		value = config[key]
-		if value == "":
-			continue
+def _WriteINI(file, config=configDefaults):
+    """
+    Writes the config dictionary to a .INI file.
+    """
+    if len(config) == 0:
+        return
+    iniFile = open(file, "w")
+    currentSection = ""
+    keys = config.keys()
+    keys.sort()
+    for key in keys:
+        # Don't write entries with no value
+        value = config[key]
+        if value == "":
+            continue
 
-		# Split the key into section and variable.		
-		section, var = string.split(key, ".", 1)
-		if section != currentSection:
-			# start a new section
-			if currentSection:
-				iniFile.write("\n")
-			iniFile.write("[%s]\n" % section)
-			currentSection = section
+        # Split the key into section and variable.		
+        section, var = string.split(key, ".", 1)
+        if section != currentSection:
+            # start a new section
+            if currentSection:
+                iniFile.write("\n")
+            iniFile.write("[%s]\n" % section)
+            currentSection = section
 
-		# write the entry
-		iniFile.write("%s = %s\n" % (var, value))
-	iniFile.close()
+        # write the entry
+        iniFile.write("%s = %s\n" % (var, value))
+    iniFile.close()
 
-def __ReadINI(file, config={}):
-	"""
-	Returns a dictionary with keys of the form
-	<section>.<option> and the corresponding values.
-	"""
-	config = config.copy()
-	cp = ConfigParser.ConfigParser()
-	cp.read(file)
-	for sec in cp.sections():
-		name = string.lower(sec)
-		for opt in cp.options(sec):
-			config[name + "." + string.lower(opt)] = string.strip(cp.get(sec, opt))
-	return config
+def _ReadINI(file, config={}):
+    """
+    Returns a dictionary with keys of the form
+    <section>.<option> and the corresponding values.
+    """
+    config = config.copy()
+    cp = ConfigParser.ConfigParser()
+    cp.read(file)
+    for sec in cp.sections():
+        name = sec.lower()
+        for opt in cp.options(sec):
+            config[name + "." + opt.lower()] = string.strip(cp.get(sec, opt))
+    return config
 
 
-__config = {}
+_config = None
 def load(file):
     """
     Returns a dictionary containing the current configuration information,
     read from the specified .INI file.
     """
-    global __config, configDefaults
+    global _config, configDefaults
     try:
         if not os.path.exists(file):
-            __WriteINI(file)
-        __config = __ReadINI(file, configDefaults)
+            _WriteINI(file)
+        _config = _ReadINI(file, configDefaults)
     except:
         print "ERROR: could not load config file %s" % file
         sys.exit(-1)
-    return __config
+    return _config
 
 def get():
-    global __config
-    return __config
+    global _config
+    if _config is None:
+        _config = load(configFileName)
+    return _config
