ec1bb4259db93a89c4e0d30fc8ae0a35497e109f
cort <cort>
2004-10-18 20:39:12 +0000

    Added ICQ front-end, based on the Twisted framework.

---
+++ howie/frontends/icq.py
@@ -0,0 +1,57 @@
+import getpass
+import random
+import time
+from twisted.internet import default
+#default.install()
+from twisted.protocols import oscar
+from twisted.internet import protocol, reactor
+
+import howie.configFile
+import frontend
+import oscarlistener
+
+# This string needs to be defined for each front end.  It should
+# contain the name of the front-end class defined in this module.
+frontEndClass = "FrontEndICQ"
+
+# default config file values for this module
+configDefaults = {
+	"icq.active":			"no",
+	"icq.screenname":		"",
+	"icq.password":			"",
+	"icq.profile":			"Your profile goes here."
+	}
+
+class FrontEndICQ(frontend.IFrontEnd):
+	def __init__(self):
+		config = howie.configFile.get()
+		try:
+			screenname = config['icq.screenname']
+			if screenname == "": raise KeyError
+		except KeyError: screenname = raw_input("ICQ screenname: ")
+		try:
+			password = config['icq.password']
+			if password == "": raise KeyError
+		except KeyError: password = getpass.getpass("ICQ password for %s: " % screenname)
+
+		hostport = ('login.icq.com', 5238)
+		#hostport = ('login.oscar.aol.com', 5190)
+
+		class OAuth(oscar.OscarAuthenticator):
+			BOSClass = oscarlistener.OscarListener
+		client = protocol.ClientCreator(reactor, OAuth, screenname, password, icq=1)
+		client.connectTCP(*hostport)
+				
+	def go(self):
+		# Serve forever
+		print "***ICQ is off an running!"
+		reactor.run(installSignalHandlers=0)
+
+	def display(self, output, user):
+		"Sends output as an IM to the specified user."
+		#self.do_SEND_IM(user, output)
+		pass
+
+# if this file is run directly...
+if __name__ == "__main__":
+	pass
