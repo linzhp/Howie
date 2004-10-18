2a492e8a5e646ff92718c8a37f92979c2a0ebccd
cort <cort>
2004-10-18 20:40:14 +0000

    Migrated AIM front-end to use Twisted framework.  Virtually all the code is shared with the ICQ front-end.

--- howie/frontends/aim.py
+++ howie/frontends/aim.py
@@ -1,10 +1,14 @@
 import getpass
 import random
 import time
-import toc
+from twisted.internet import default
+#default.install()
+from twisted.protocols import oscar
+from twisted.internet import protocol, reactor
 
 import howie.configFile
 import frontend
+import oscarlistener
 
 # This string needs to be defined for each front end.  It should
 # contain the name of the front-end class defined in this module.
@@ -18,7 +22,7 @@ configDefaults = {
 	"aim.profile":			"Your profile goes here."
 	}
 
-class FrontEndAIM(toc.TocTalk, frontend.IFrontEnd):
+class FrontEndAIM(frontend.IFrontEnd):
 	def __init__(self):
 		config = howie.configFile.get()
 		try:
@@ -29,123 +33,26 @@ class FrontEndAIM(toc.TocTalk, frontend.IFrontEnd):
 			password = config['aim.password']
 			if password == "": raise KeyError
 		except KeyError: password = getpass.getpass("AIM password for %s: " % screenname)
-		toc.TocTalk.__init__(self, screenname, password)
-		try: self._info = config['aim.profile']
-		except KeyError: self._info = "No profile."
-		try: self._maxdelay = int(config['general.maxdelay'])
-		except KeyError: self._maxdelay = 0
 
-	def start(self):
-		"Do stuff here immediately after the bot connects."
-		# Placeholder.
-		pass
+		#hostport = ('login.icq.com', 5238)
+		hostport = ('login.oscar.aol.com', 5190)
+
+		class OAuth(oscar.OscarAuthenticator):
+			BOSClass = oscarlistener.OscarListener
+		print "*** AIM: creating client."
+		client = protocol.ClientCreator(reactor, OAuth, screenname, password, icq=0)
+		print "*** AIM: connecting to server."
+		client.connectTCP(*hostport)
+		print "*** AIM: all is good!"
+				
+	def go(self):
+		# Serve forever
+		print "***AIM is off an running!"
+		reactor.run(installSignalHandlers=0)
 
 	def display(self, output, user):
 		"Sends output as an IM to the specified user."
-		self.do_SEND_IM(user, output)
-
-	def _updateConfig(self):
-		"Saves our current configuration (buddy list, etc.) back to the server."
-		# Create a TOC-style CONFIG string
-		newConfig = "m 1\ng Buddies\n"
-		for buddy in self._buddyList:
-			newConfig += "b " + buddy + "\n"
-		self.do_SET_CONFIG(newConfig)
-
-	def _addBuddy(self, newBuddy):
-		"Add a new buddy, both locally and remotely."
-		if self._buddyList.count(newBuddy) > 0:
-			# Buddy already in list.
-			return
-		self.do_ADD_BUDDY(list(newBuddy))
-		self._buddyList.append(newBuddy)
-		self._updateConfig()
-
-	def _removeBuddy(self, oldBuddy):
-		"Remove an existing buddy from the buddy list."
-		if self._buddyList.count(oldBuddy) == 0:
-			# No such buddy.
-			return
-		self.do_REMOVE_BUDDY(list(oldBuddy))
-		self._buddyList.remove(oldBuddy)
-		self._updateConfig()
-
-	### AIM event handlers follow
-
-	# Called whenever someone IMs us.
-	# data contains "screenname:flag:message", such as
-	#   "jamwt:F:hey, ben.. how's work?".
-	# flag is "T" if the message was sent as an auto-response, "F"
-	# otherwise.
-	def on_IM_IN(self, data):
-		# we must use the optional maxsplit argument of 2 in case the
-		# message contains a colon!
-		data_components = data.split(":",2)
-
-		# Store screen name.
-		screenname = data_components[0]  # the sender's screenname
-
-		# Store whether the message is an auto-response.
-		bAutoResponse = (data_components[1] == "T")
-
-		# Some clients send messages as HTML.  We don't give a damn.
-		message = self.strip_html(data_components[2])
-
-		# Send a reply.
-		# Insert a random delay of 0-maxdelay seconds before replying.
-		if not bAutoResponse:
-			time.sleep( random.random() * self._maxdelay )
-			response = self.submit(message, screenname+"@AIM")
-			self.display(response, screenname)
-
-		
-	# Called when there's an incoming user configuration (our
-	# buddy list).  Can be empty.  Format of data : The config
-	# information is a string containing a series of lines with
-	# the first character in each being the item type, followed by
-	# a space, with the rest of the line being the item
-	# value. Only letters, numbers, and spaces should be used.
-	def on_CONFIG(self, data):
-		# first time logging in--add buddies from config...
-		self._buddyList = []
-		budsToAdd = []
-		
-		# remember the format of config data here:
-		# "m 1\ng Buddies\nb bouncebot\nb perlaim\n"
-		for item in data.split("\n"):
-			if item == '':
-				continue
-			if item[0] == "b":
-				budsToAdd.append(item[1:].strip())
-				
-				
-			#add no more than ~20 at a time, msg len restrictions
-			if len(budsToAdd) == 20:
-				self.do_ADD_BUDDY(budsToAdd)
-				self._buddyList.extend(budsToAdd)
-				time.sleep(0.2) # don't SLAM the server...
-				budsToAdd = []
-
-		# Clean up any leftovers.
-		if len(budsToAdd) > 0:
-			self.do_ADD_BUDDY(budsToAdd)
-			self._buddyList.extend(budsToAdd)
-			budsToAdd = []
-
-	# This command handles arrivals, departures, and updates of
-	# members on your bots buddy list.
-	#
-	# Format of data : Colon delimited; first is screenname, then
-	# T/F for online status, warning percentage, time of sign-on
-	# in Unix epoc format, idle time in minutes, and finally a
-	# "User Class" string of 2 or three characters.
-	def on_UPDATE_BUDDY(self, data):
-		data_components = data.split(":", 5)
-
-	# Some random events I don't understand yet...
-	def on_NOID(self, data):
-		pass
-	def on_NICK(self, data):
+		#self.do_SEND_IM(user, output)
 		pass
 
 # if this file is run directly...
